16946d85b3a8badef6aae36f1ae4dd65dc97c71b
keshariamudit24 <keshariamudit@gmail.com>
2024-10-22 15:25:57 +0530

    smart contract deployment

--- smart_contracts/contracts/Transactions.sol
+++ smart_contracts/contracts/Transactions.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.27;
 
 contract Transactions{
 
@@ -20,7 +20,7 @@ contract Transactions{
     TransferStruct[] transactions;
 
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
-        
+
         transactionCount += 1;
         transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-sepolia.g.alchemy.com/v2/x7w8_UWU14BVf-sli14InlDfBsN9SBAu
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.8.27",
-};
+  solidity: '0.8.27',
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/x7w8_UWU14BVf-sli14InlDfBsN9SBAu",
+      accounts: ['50a93019a09c4e56d7a4f7a625058e4c3092b55e25754e16a2ec31519dce81a4']
+    }
+  }
+}
