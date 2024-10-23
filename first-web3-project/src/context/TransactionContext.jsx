f9043763f5b850845b30480ad68c02d802937798
keshariamudit24 <keshariamudit@gmail.com>
2024-10-23 17:11:04 +0530

    error fixed

--- first-web3-project/src/context/TransactionContext.jsx
+++ first-web3-project/src/context/TransactionContext.jsx
@@ -5,9 +5,9 @@ import { contractAddress, contractABI } from '../utils/constants';
 
 export const TransactionContext = React.createContext();
 
-const { ethereum } = window;
-
 const getEthereumContract = () => {
+    const { ethereum } = window; // Destructure here, inside the function
+
     if (!ethereum) {
         console.log("No Ethereum object found. Make sure you have MetaMask installed.");
         return null;
@@ -26,64 +26,67 @@ const getEthereumContract = () => {
     return transactionContract;
 }
 
-export const TransactionProvider = ({ children }) =>{
+export const TransactionProvider = ({ children }) => {
 
     const [currentAccount, setCurrentAccount] = useState('');
-    const[formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: ''});
+    const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: '' });
 
     const handleChange = (e, name) => {
-        setFormData((prevState) => ({ ...prevState, [name]: e.target.value}));
+        setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
     }
 
     const checkIfWalletIsConnected = async () => {
-        try{
-            if(!ethereum) return alert("Please Install Metamask");
+        try {
+            const { ethereum } = window; // Destructure inside this function
+
+            if (!ethereum) return alert("Please Install MetaMask");
 
             const accounts = await ethereum.request({ method: 'eth_accounts' });
-            
-            if(accounts.length){
-                setCurrentAccount(accounts[0]);
-            }
-            else{
-                console.log("No accounts found")
-            }
-        }
-        catch(error){
-            console.log(error);
 
-            throw new Error("No ethereum object.")
+            if (accounts.length) {
+                setCurrentAccount(accounts[0]);
+            } else {
+                console.log("No accounts found");
+            }
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.");
         }
     }
 
-    const connectWallet = async() => {
-        try{
-            if(!ethereum) return alert("Please Install Metamask");
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window; // Destructure inside this function
+
+            if (!ethereum) return alert("Please Install MetaMask");
 
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 
             setCurrentAccount(accounts[0]);
-        } catch(error){
+        } catch (error) {
             console.log(error);
-
-            throw new Error("No ethereum object.")
+            throw new Error("No ethereum object.");
         }
     }
 
-    const sendTransaction = async() =>{
-        try{
-            if(!ethereum) return alert("Please Install Metamask");
-            
-            //get data from the form
-            const { addressTo, amount, keyword, message} = formData;
+    const sendTransaction = async () => {
+        try {
+            const { ethereum } = window; // Destructure inside this function
+
+            if (!ethereum) return alert("Please Install MetaMask");
+
+            // Get data from the form
+            const { addressTo, amount, keyword, message } = formData;
             const transactionContract = getEthereumContract();
 
             if (!transactionContract) {
                 return console.log("No Ethereum contract found.");
             }
-        }catch(error){
-            console.log(error);
 
-            throw new Error("No ethereum object.")
+            // You can add the rest of your transaction logic here
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object.");
         }
     }
 
@@ -91,9 +94,9 @@ export const TransactionProvider = ({ children }) =>{
         checkIfWalletIsConnected();
     }, []);
 
-    return(
+    return (
         <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction }}>
-        {children}
+            {children}
         </TransactionContext.Provider>
     )
 }
