b0e961554bc836f433b57db3ae0040b3d88685ae
keshariamudit24 <keshariamudit@gmail.com>
2024-10-23 17:49:54 +0530

    able to make transactions

--- first-web3-project/src/context/TransactionContext.jsx
+++ first-web3-project/src/context/TransactionContext.jsx
@@ -17,11 +17,11 @@ const getEthereumContract = () => {
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    console.log({
-        provider, 
-        signer,
-        transactionContract
-    });
+    // console.log({
+    //     provider, 
+    //     signer,
+    //     transactionContract
+    // });
 
     return transactionContract;
 }
@@ -30,6 +30,8 @@ export const TransactionProvider = ({ children }) => {
 
     const [currentAccount, setCurrentAccount] = useState('');
     const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: '' });
+    const [isloading, setIsLoading] = useState(false);
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
 
     const handleChange = (e, name) => {
         setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
@@ -78,12 +80,35 @@ export const TransactionProvider = ({ children }) => {
             // Get data from the form
             const { addressTo, amount, keyword, message } = formData;
             const transactionContract = getEthereumContract();
+            const parsedAmount = ethers.utils.parseEther(amount);
 
             if (!transactionContract) {
                 return console.log("No Ethereum contract found.");
             }
 
-            // You can add the rest of your transaction logic here
+            await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [{
+                    from: currentAccount,
+                    to: addressTo,
+                    gas: '0x5208', //21000 GWEI
+                    value: parsedAmount._hex, //0.0001 ETH (_hex converts this eth to hex gwei)
+                }]
+            });
+
+
+            //asynchronous, gotta add a loading state --> line no. 33
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+            
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait();
+            setIsLoading(false);
+            console.log(`Success - ${transactionHash.hash}`);
+
+            const transactionCount = await transactionContract.getTransactionCount();
+            setTransactionCount(transactionCount.toNumber());
+
         } catch (error) {
             console.log(error);
             throw new Error("No ethereum object.");
